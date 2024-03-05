5200b1e5421fb74a819a952d98a3edfc1eaeaae0
Your Name <you@example.com>
2024-03-05 13:24:07 +0800

    1.1

--- autosuggestai.php
+++ autosuggestai.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AutoSuggestAI
  * Description: Auto suggest text in the block editor using AI
- * Version: 1.0
+ * Version: 1.1
  * Author: Raymond Lowe
  * License: GPL2
  * URL: https://github.com/raymondclowe/AutoSuggestAI
