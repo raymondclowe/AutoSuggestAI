ac832485a935fdee2c0b4159feba4eb48a8af66e
Raymond Lowe <raymondclowe@gmail.com>
2024-03-05 09:44:15 +0800

    0.5 start

--- autosuggestai.php
+++ autosuggestai.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AutoSuggestAI
  * Description: Auto suggest text in the block editor using AI
- * Version: 0.4
+ * Version: 0.5
  * Author: Raymond Lowe
  * License: GPL2
  * URL: https://github.com/raymondclowe/AutoSuggestAI
