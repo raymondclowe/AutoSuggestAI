2bc5aa135818ca30c628f2bca247084a438e89a3
Your Name <you@example.com>
2024-08-22 15:26:12 +0800

    centralize prompt text

--- autosuggestai.php
+++ autosuggestai.php
@@ -4,7 +4,7 @@
  * Plugin Name: AutoSuggestAI
  * Plugin URI: https://github.com/raymondclowe/AutoSuggestAI
  * Description: Auto suggest text in the block editor using AI
- * Version: v2.4.5
+ * Version: v2.5.0
  * Author: Raymond Lowe 
  * Author URI: https://github.com/raymondclowe/
  * Text Domain: AutoSuggestAI
@@ -25,11 +25,11 @@ defined('ABSPATH') or die('No script kiddies please!');
 // ini_set('error_log', $errorLog);
 
 
-function autosuggestai_enqueue_scripts()
-{
-  wp_enqueue_script('autosuggestai', plugins_url('autosuggestai.js', __FILE__), array(), 'v2.4.5');
+function autosuggestai_enqueue_scripts(){
+  wp_enqueue_script('autosuggestai', plugins_url('autosuggestai.js', __FILE__), array(), 'v2.5.0');
   wp_localize_script('autosuggestai', 'autosuggestai', array(
     'api_nonce' => wp_create_nonce('wp_rest'),
+    'promptTemplate' => $thePrompt
   ));
 }
 
@@ -249,36 +249,7 @@ global $errorLog;
 
 $promptTemplate = "[INST] {prompt} [/INST]"; // <s> only needed for multi turn
 
-$thePrompt = <<<EOD
-You are an automated writing assistant who will suggest the next piece of text to write after an example given to you. The suggested text you provide will always be brief, meaningful, sensible, in keeping with the style of the example. You will be given the title of the piece of writing, one or more preceding paragraphs, and the incomplete paragraph that needs extending. The place to add text will be marked with three dots like this ...
-
-
-Examples:
-
-If you are given the text "The cat sat on the ... " then you will reply "mat."
-If you are given the text "The rain in Spain falls ... " then you will reply with "mainly on the plain. In Hertford, Hereford, and Hampshire, hurricanes hardly ever happen.".
-If you are given the text "The sun rises in the east ... " then you will reply with "and sets in the west".
-If you are given the text "The dog ... " then you will reply with "barks, and the cat meows".
-If you are given the text "Four score and seven years ago ... " then you will reply with "our fathers brought forth, upon this continent, a new nation".
-
-
-You will return only the suggested extention of the text, without including the original incomplete paragraph.
-
-
-Here is the title of the piece of writing:
-
-{title}
-
-
-Here are the preceding paragraphs for context:
-
-{context}
-
-
-Here is the text to be extended:
-
-{text}
-EOD;
+$thePrompt = file_get_contents(__DIR__ . '/prompt_template.txt');
 
 
 // error_log("template will be  : " . $promptTemplate . "\n", 3, $errorLog);
